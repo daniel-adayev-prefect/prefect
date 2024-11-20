0e9283bdb3af049436b3ab45b248989c4a21ef99
daniel-adayev-prefect <daniel.adayev@prefect.io>
2024-11-20 12:17:26 -0800

    Update weather_managed.py

--- weather_managed.py
+++ weather_managed.py
@@ -1,10 +1,16 @@
-from prefect import flow, task, runtime
-
-
-
+import pandas as pd
+from prefect import flow
 
 @flow(log_prints=True)
-def fetch_weather_v2():
-    url = "https://api.open-meteo.com/v1/forecast"
-    print(url)
+def run_pandas():
+    data = {
+        'Name': ['Alice', 'Bob', 'Charlie', 'David', 'Eva'],
+        'Age': [24, 27, 22, 32, 29],
+        'City': ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix']
+    }
 
+    # Create a pandas DataFrame from the dictionary
+    df = pd.DataFrame(data)
+
+    # Print the first 5 lines of the DataFrame
+    print(df.head())
