52e82804659d9ebadeaddace4231afc8e2c1024a
daniel-adayev-prefect <daniel.adayev@prefect.io>
2024-11-20 12:09:53 -0800

    Update weather_managed.py

--- weather_managed.py
+++ weather_managed.py
@@ -3,9 +3,8 @@ from prefect import flow, task, runtime
 
 
 
-@flow
+@flow(log_prints=True)
 def fetch_weather_v2():
     url = "https://api.open-meteo.com/v1/forecast"
     print(url)
-    return url
 
